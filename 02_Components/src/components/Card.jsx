00db0752a71789779211397ad1c421fa5ee40c60
Ritwik <ritwikhero@gmail.com>
2025-10-11 17:33:50 +0530

    react folder structure and components completed

---
+++ 02_Components/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <h1>Ritwik</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque,
+        voluptatibus.
+      </p>
+    </div>
+  );
+};
+
+export default Card;
