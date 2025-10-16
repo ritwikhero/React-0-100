c89c734b0e20f178c9d93b899b519c846cee7fdf
Ritwik <ritwikhero@gmail.com>
2025-10-17 00:28:11 +0530

    work in progress

--- 07_UI_Project/src/components/Section1/Section1.jsx
+++ 07_UI_Project/src/components/Section1/Section1.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Page1Content from "./Page1Content";
 
 const Section1 = () => {
   return (
-    <div className="h-screen w-full bg-amber-700">
+    <div className="h-screen w-full bg-gray-400">
       <Navbar />
+      <Page1Content />
     </div>
   );
 };
